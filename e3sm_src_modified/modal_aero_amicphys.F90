39ae3de6119e967be3b448c2c1278b71751ca858
laura.fierce@pnnl.gov <laura.fierce@pnnl.gov>
2025-10-29 19:53:25 -0700

    set accom_coef_gas = 1.0 in modl_aero_amicrophys.F90 to be consistent with PartMC

--- e3sm_src_modified/modal_aero_amicphys.F90
+++ e3sm_src_modified/modal_aero_amicphys.F90
@@ -5735,8 +5735,9 @@ dr_so4_monolayers_pcage = n_so4_monolayers_pcage * 4.76e-10
       fcvt_gas(:) = 1.0_r8
 
       vol_molar_gas = 42.88_r8  ! value for h2so4
-      accom_coef_gas = 0.65_r8  ! value for h2so4
-
+      ! accom_coef_gas = 0.65_r8  ! value for h2so4
+      accom_coef_gas = 1.0_r8  ! value for h2so4
+      
       do igas = 1, ngas
          call cnst_get_ind( name_gas(igas), l, .false. )
          if (l < 1 .or. l > pcnst) then
